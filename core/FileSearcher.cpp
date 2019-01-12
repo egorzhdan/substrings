8c13126adfadc7ad11e091103caf2b07170d967e
Egor Zhdan <egor.zhdan@jetbrains.com>
2019-01-12 12:16:26 +0300

    Better searching

--- core/FileSearcher.cpp
+++ core/FileSearcher.cpp
@@ -1,3 +1,4 @@
+#include <QTextStream>
 #include "FileSearcher.h"
 
 FileSearcher::FileSearcher(const QString& filePath, const QSet<Index::HashWord>& trigrams, Searcher *parent) : filePath(filePath), trigrams(trigrams), parent(parent) {}
@@ -9,12 +10,32 @@ void FileSearcher::run() {
 }
 
 bool FileSearcher::contains(const QString &filePath, const QSet<Index::HashWord> &trigrams) {
-    for (auto &x : trigrams) {
-        const auto &hash = parent->index.get(filePath);
+    const QString &query = parent->query;
+    if (query.size() < 3) {
+        for (auto &x : trigrams) {
+            const auto &hash = parent->index.get(filePath);
 
-        if (!std::binary_search(hash.begin(), hash.end(), x)) {
-            return false;
+            if (!std::binary_search(hash.begin(), hash.end(), x)) {
+                return false;
+            }
         }
     }
-    return true;
+
+    const auto BUFFER_SIZE = std::max(1000, query.size() + 1);
+    QFile file(filePath);
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QTextStream stream(&file);
+        QString oldBuffer = stream.read(BUFFER_SIZE);
+        QString newBuffer = stream.read(BUFFER_SIZE);
+        do {
+            if ((oldBuffer + newBuffer).contains(query)) {
+                file.close();
+                return true;
+            }
+            oldBuffer = newBuffer;
+            newBuffer = stream.read(BUFFER_SIZE);
+        } while (!newBuffer.isEmpty());
+    }
+    file.close();
+    return false;
 }
